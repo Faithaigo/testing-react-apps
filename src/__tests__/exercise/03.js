2f59ea6f96d78352953ceeb8a50476a82b2530e1
faith <faith@marascientific.org>
2021-11-26 20:48:04 +0300

    Replace fireEvent with userEvent

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -5,11 +5,11 @@ import * as React from 'react'
 // 🐨 add `screen` to the import here:
 import {render, fireEvent, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
+import userEvent from '@testing-library/user-event'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
+  render(<Counter />)
+
   
   const increment = screen.getByRole('button',{name:'Increment'})
    
@@ -17,8 +17,8 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const message = screen.getByText('Current count: 0')
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
