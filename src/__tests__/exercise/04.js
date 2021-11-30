244456d0e7181e20337dd194d5e6331637cf6b23
faith <faith@marascientific.org>
2021-11-30 07:57:16 +0300

    Generate random test data using faker

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,19 +4,28 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Login from '../../components/login'
+import Login from '../../components/login';
+import faker from 'faker';
+
+
+const buildLoginForm = () =>{
+  return {
+    username:faker.internet.userName(),
+    password:faker.internet.password()
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   //let submittedData;
   //const handleSubmit = data =>(submittedData=data)
+  
+  const {username,password} = buildLoginForm()
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit}/>)
-  const userName = screen.getByLabelText(/Username/i)
-  const password = screen.getByLabelText(/Password/i)
-  userEvent.type(userName,'Faith')
-  userEvent.type(password,'password1');
+  userEvent.type(screen.getByLabelText(/Username/i),username)
+  userEvent.type(screen.getByLabelText(/Password/i),password);
   userEvent.click(screen.getByText(/Submit/i))
-  expect(handleSubmit).toHaveBeenCalledWith({username:'Faith',password:'password1'})
+  expect(handleSubmit).toHaveBeenCalledWith({username,password})
 })
 
 /*
