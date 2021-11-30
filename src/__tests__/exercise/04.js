9412a14fa2d1ff34aa7ba4f8debd310c80687cf3
faith <faith@marascientific.org>
2021-11-30 08:10:38 +0300

    use test-data-bot to generate random data

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,21 +6,20 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login';
 import faker from 'faker';
+import {build,fake} from '@jackfranklin/test-data-bot'
 
 
-const buildLoginForm = (overides) =>{
-  return {
-    username:faker.internet.password(),
-    password:faker.internet.userName(),
-    ...overides
-  }
-}
+const buildLoginForm =  build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password:fake(f => f.internet.password()),
+  },
+});
 
 test('submitting the form calls onSubmit with username and password', () => {
   //let submittedData;
   //const handleSubmit = data =>(submittedData=data)
-
-  const {username,password} = buildLoginForm({password:'password1'})
+  const {username,password} = buildLoginForm()
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit}/>)
   userEvent.type(screen.getByLabelText(/Username/i),username)
